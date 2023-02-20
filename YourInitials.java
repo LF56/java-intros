8add603b2912d24b71539733361a216f98e4bfbf
LF56 <leah.fox7@gmail.com>
2023-02-20 17:19:33 -0500

    added letter f with correct spacing

--- YourInitials.java
+++ YourInitials.java
@@ -1,12 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
-System.out.println("L");
-System.out.println("L");
-System.out.println("L");
-System.out.println("L");
-System.out.println("L");
-System.out.println("L");
-System.out.println("L L L L L");
+System.out.println("L            F F F F F");
+System.out.println("L            F");
+System.out.println("L            F");
+System.out.println("L            F F F");
+System.out.println("L            F");
+System.out.println("L            F");
+System.out.println("L L L L L    F");
 
     }
 }
