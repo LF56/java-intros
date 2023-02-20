1b0fd3f2e83275daa90f469e51190b6c561af967
LF56 <leah.fox7@gmail.com>
2023-02-20 18:28:26 -0500

    added -test function for minus opperator

---
+++ section two/Points.java
@@ -0,0 +1,9 @@
+public class Points {
+    public static void main(String[] args) {
+        int points = 0 ;
+        System.out.println("Harry was caught wantering in the halls. -50 points for Gryffindor");
+        points -= 50;
+        System.out.println(points);
+
+    }
+}
