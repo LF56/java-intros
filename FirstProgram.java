d2adf4cb2c5dca9d1e8f62a833461b8a90fe243c
LF56 <leah.fox7@gmail.com>
2023-02-20 14:36:27 -0500

    added workbook one instructions

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+// I'm a comment. I have no impact on your code.
+// Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+//Task include: make a class
+// Write the main method
+// Inside the main method, pring the following to the console:
+// >> Printing is fun!
+// >> Java > Python.
+// >> I spilled Java all over my paper.
+// >> My dog ate my Java.
