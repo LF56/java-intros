840a5975c85a79d3a330e45be62dabec54a1d424
LF56 <leah.fox7@gmail.com>
2023-02-22 14:41:00 -0500

    added test outputs and fixed spaces

---
+++ section two/Biography.java
@@ -0,0 +1,30 @@
+public class Biography {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+        String name = "Leah";
+        String county = "United States of America";
+        String sport = "gymnastics";
+        String game = "Mario Kart";
+        String subject = "math";
+        int age = 22; 
+        int hours = 6;
+        char grade = 'A';
+
+        // name (text)
+        // age (whole number)
+        // country (text)
+        // sport (text)
+        // hours (whole number)
+        // game (text)
+        // subject (text)
+        // grade (character)
+     
+        System.out.println("My name is "  + name +  " I'm " + age + " years old, and I'm from " + county);
+        System.out.println("My favorite sport is" + sport + "I play for" + hours + "hours a day");
+        System.out.println("When I'm tired, I like to play <game>.");
+        System.out.println("In school, my favorite subject was <subject>, I scored a <grade>.");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
---
+++ section two/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithnacy = 'B';
+        char charms = 'C';
+
+        System.out.println("Potions " + potions);
+        System.out.println("Arithnacy " + arithnacy);
+        System.out.println("Charms " + charms);
+    }
+}
